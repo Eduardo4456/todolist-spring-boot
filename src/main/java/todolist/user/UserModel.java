71494465740aae3a9f60f9587afdc427144cc811
Eduardo4456 <eduardosantn060705@gmail.com>
2026-09-03 19:01:40 -0300

    conectando com banco de dados e adicionando UserRepository

--- src/main/java/todolist/user/UserModel.java
+++ src/main/java/todolist/user/UserModel.java
@@ -1,12 +1,55 @@
 package todolist.user;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
-import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
-@Data
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_users")
 public class UserModel {
     
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
     private String userName;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
 }
